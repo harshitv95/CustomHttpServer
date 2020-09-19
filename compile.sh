1d0cda88bb7658df02ffeaca717551185720eb83
Harshit Vadodaria <harshitv95@gmail.com>
2020-09-18 21:47:14 -0400

    +Compile script

---
+++ compile.sh
@@ -0,0 +1,16 @@
+find -name "*.java" > sources.txt
+javac -d bin/ @sources.txt
+
+PACKAGE_ROOT=$(echo "$dirs")
+
+rm -f Manifest.mf
+echo "Manifest-Version: 1.0" >> Manifest.mf
+echo "Main-Class: com.hvadoda1.server.starter.ServerStarter" > Manifest.mf
+
+cd bin/
+jar cmf ../Manifest.mf ../HttpServer.jar com/
+
+cd ..
+rm -f Manifest.mf
+rm -f sources.txt
+rm -rf bin
